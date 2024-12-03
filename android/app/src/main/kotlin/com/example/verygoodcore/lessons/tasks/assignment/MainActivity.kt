482f72e8229202a0732f2e091ab0ee8909bc109e
Peter Trost <p.trost93@gmail.com>
2024-12-03 17:24:44 +0100

    feat: Add basic app

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/lessons/tasks/assignment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.lessons_tasks_assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:lessons_tasks_assignment/app/app.dart';
+import 'package:lessons_tasks_assignment/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
