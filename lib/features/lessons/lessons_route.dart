9c8ef8ec19058149344a0258d2164270b57df156
Peter Trost <p.trost93@gmail.com>
2024-12-09 09:33:53 +0100

    refactor: Use GetIt to inject dependencies

--- lib/features/lessons/lessons_route.dart
+++ lib/features/lessons/lessons_route.dart
@@ -1,10 +1,8 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 import 'package:lessons_tasks_assignment/data/repositories/lessons/lessons_repository.dart';
-import 'package:lessons_tasks_assignment/data/services/interceptors/json_interceptor.dart';
-import 'package:lessons_tasks_assignment/data/services/rest_client.dart';
 import 'package:lessons_tasks_assignment/features/lesson/lesson_route.dart';
 import 'package:lessons_tasks_assignment/features/lessons/cubit/lessons_cubit.dart';
 import 'package:lessons_tasks_assignment/features/lessons/view/lessons_view.dart';
@@ -32,11 +30,10 @@ part 'lessons_route.g.dart';
 class LessonsRoute extends GoRouteData {
   @override
   Widget build(BuildContext context, GoRouterState state) {
-    final lessonsRepository = LessonsRepository(
-      restClient: RestClient(Dio()..interceptors.add(JsonInterceptor())),
-    );
     return BlocProvider(
-      create: (context) => LessonsCubit(lessonsRepository)..fetchLessons(),
+      create: (context) => LessonsCubit(
+        GetIt.I<LessonsRepository>(),
+      )..fetchLessons(),
       child: const LessonsView(),
     );
   }
--- test/integration/app_test.dart
+++ test/integration/app_test.dart
@@ -1,9 +1,14 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:lessons_tasks_assignment/app/view/app.dart';
+import 'package:lessons_tasks_assignment/data/repositories/lessons/lessons_repository.dart';
+import 'package:lessons_tasks_assignment/data/repositories/tasks/tasks_repository.dart';
+import 'package:lessons_tasks_assignment/data/services/interceptors/json_interceptor.dart';
+import 'package:lessons_tasks_assignment/data/services/rest_client.dart';
 import 'package:lessons_tasks_assignment/features/lesson/view/lesson_view.dart';
 import 'package:lessons_tasks_assignment/features/lesson/view/widgets/pages_view.dart';
 import 'package:lessons_tasks_assignment/features/lessons/view/lessons_view.dart';
@@ -29,10 +34,20 @@ void main() {
     ).thenAnswer(
       (_) => const Stream<FileResponse>.empty(),
     );
+    final dio = Dio()..interceptors.add(JsonInterceptor());
+    final restClient = RestClient(dio);
+    GetIt.I.registerSingleton<LessonsRepository>(
+      LessonsRepository(restClient: restClient),
+    );
+    GetIt.I.registerSingleton<TasksRepository>(
+      TasksRepository(restClient: restClient),
+    );
     GetIt.I.registerSingleton<BaseCacheManager>(cacheManager);
   });
 
   tearDown(() async {
+    GetIt.I.unregister<LessonsRepository>();
+    GetIt.I.unregister<TasksRepository>();
     GetIt.I.unregister<BaseCacheManager>();
   });
 
