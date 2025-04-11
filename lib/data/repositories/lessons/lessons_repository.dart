36183b0c6c31ad03c10b4aaedd72b109c7a1128a
Peter Trost <p.trost93@gmail.com>
2025-04-11 19:07:04 +0200

    refactor: rename repositories

--- lib/data/repositories/lessons/lessons_repository.dart
+++ lib/data/repositories/lessons/lessons_repository.dart
@@ -1,14 +1,14 @@
 import 'package:lessons_tasks_assignment/data/services/rest_client.dart';
 import 'package:lessons_tasks_assignment/domain/lesson.dart';
 
-class LessonsRepository {
-  LessonsRepository({
+class ConceptsRepository {
+  ConceptsRepository({
     required this.restClient,
   });
 
   final RestClient restClient;
 
-  Future<List<Lesson>> getLessons() async {
-    return restClient.getLessons();
+  Future<List<Concept>> getConcepts() async {
+    return restClient.getConcepts();
   }
 }
